0733cf871e99718d5dcf540a6f0ff8742e54680d
emile <emile@dirigera.se>
2025-09-24 11:39:57 +0200

    URL decoder

---
+++ URL Decoder/URLDecoder.pas
@@ -0,0 +1,55 @@
+﻿unit URLDecoder;
+
+interface
+
+uses
+  System.Classes,
+  System.Generics.Collections,
+  System.NetEncoding;
+
+type
+  TURLDecoder = class
+  private
+    FInputs: TList<string>;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    procedure AddInput(AInput: string);
+    procedure DecodeAll;
+    procedure DecodeUrl(AURL: string);
+  end;
+
+implementation
+
+{ TURLDecoder }
+
+procedure TURLDecoder.AddInput(AInput: string);
+begin
+  FInputs.Add(AInput);
+end;
+
+constructor TURLDecoder.Create;
+begin
+  FInputs := TList<string>.Create;
+end;
+
+procedure TURLDecoder.DecodeAll;
+var
+  LString: string;
+begin
+  for LString in FInputs do
+    DecodeUrl(LString);
+end;
+
+procedure TURLDecoder.DecodeUrl(AURL: string);
+begin
+  Writeln(TNetEncoding.URL.Decode(AURL));
+end;
+
+destructor TURLDecoder.Destroy;
+begin
+  FInputs.Free;
+  inherited;
+end;
+
+end.
---
+++ URL Decoder/urldec.dpr
@@ -0,0 +1,48 @@
+﻿program urldec;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+
+uses
+  System.SysUtils,
+  URLDecoder in 'URLDecoder.pas';
+
+var
+  i: Integer;
+  LDecoder: TURLDecoder;
+  LUserInput: string;
+
+begin
+  LDecoder := TURLDecoder.Create;
+  try
+    try
+      if ParamCount >= 1 then
+      begin
+        for i := 1 to ParamCount do
+          LDecoder.AddInput(ParamStr(i));
+
+        LDecoder.DecodeAll;
+      end
+      else
+      begin
+        Writeln('Enter URLs (type "quit" to exit):');
+        repeat
+          Write('> ');
+          Readln(LUserInput);
+          if LowerCase(LUserInput) <> 'quit' then
+          begin
+            LDecoder.DecodeUrl(LUserInput);
+          end;
+        until LowerCase(LUserInput) = 'quit';
+      end;
+    except
+      on E: Exception do
+        Writeln(E.ClassName, ': ', E.Message);
+    end;
+  finally
+    LDecoder.Free;
+  end;
+
+end.
