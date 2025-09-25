996c9447df7ef33ceb7ae4761d00b74c906c7b18
emile <emile@dirigera.se>
2025-09-25 10:19:39 +0200

    ip tool (unfinished), images, sublime text

---
+++ IP/IPManager.pas
@@ -0,0 +1,50 @@
+﻿unit IPManager;
+
+interface
+
+type
+  TIPManager = class
+  private
+    FInputs: TList<string>;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    procedure AddInput(AInput: string);
+    procedure GetAllIPs;
+    procedure GetIP(AHostName: string);
+  end;
+
+implementation
+
+{ TIPManager }
+
+procedure TIPManager.AddInput(AInput: string);
+begin
+  FInputs.Add(AInput);
+end;
+
+constructor TIPManager.Create;
+begin
+  FInputs := TList<string>.Create;
+end;
+
+destructor TIPManager.Destroy;
+begin
+  FInputs.Free;
+  inherited;
+end;
+
+procedure TIPManager.GetAllIPs;
+var
+  LString: string;
+begin
+  for LString in FInputs do
+    GetIP(LString);
+end;
+
+procedure TIPManager.GetIP(AHostName: string);
+begin
+
+end;
+
+end.
