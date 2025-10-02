1eb85998db1570dd6481dedc0c4c304d44848e5b
emile <emile@dirigera.se>
2025-10-02 22:57:56 +0200

    Icons, base64

---
+++ base64/Encoder.pas
@@ -0,0 +1,70 @@
+﻿unit Encoder;
+
+interface
+
+uses
+  System.NetEncoding;
+
+type
+  TEncoder = class
+  public
+    class procedure Encode(ALine: string); static;
+    class procedure Decode(ALine: string); static;
+  end;
+
+implementation
+
+
+
+{ TURLDecoder }
+
+constructor TEncoder.Create;
+begin
+  FNetEncoding := TNetEncoding.Create;
+end;
+
+destructor TEncoder.Destroy;
+begin
+  FNetEncoding.Free;
+  inherited;
+end;
+
+procedure TEncoder.DoDecode(ALine: string);
+begin
+  Writeln(FNetEncoding.Encode(ALine));
+end;
+
+procedure TEncoder.DoEncode(ALine: string);
+begin
+   Writeln(FNetEncoding.Decode(ALine));
+end;
+
+{ Static }
+
+class procedure TEncoder.Decode(ALine: string);
+var
+  LEncoder: TEncoder;
+begin
+  LEncoder := TEncoder.Create;
+  try
+    LEncoder.Decode(Aline);
+  finally
+    LEncoder.Free;
+  end;
+end;
+
+
+
+class procedure TEncoder.Encode(ALine: string);
+var
+  LEncoder: TEncoder;
+begin
+  LEncoder := TEncoder.Create;
+  try
+    LEncoder.Encode(Aline);
+  finally
+    LEncoder.Free;
+  end
+end;
+
+end.
---
+++ base64/base64.dpr
@@ -0,0 +1,92 @@
+﻿program base64;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+
+uses
+  System.NetEncoding,
+  System.SysUtils;
+
+function ParamIsEncode(AParameter: string): Boolean;
+begin
+  Result := SameText(AParameter, 'e') or SameText(AParameter, '-e') or SameText(AParameter, '--encode');
+end;
+
+function ParamIsDecode(AParameter: string): Boolean;
+begin
+  Result := SameText(AParameter, 'd') or SameText(AParameter, '-d') or SameText(AParameter, '--decode');
+end;
+
+procedure ShowHelp;
+begin
+  Writeln('Usage:');
+  Writeln('-e, --encode <string>: Base64 encodes a string.');
+  Writeln('-d, --decode <string>: Base64 decodes a string.');
+  Writeln('No parameter: interactive encode/decode.');
+end;
+
+var
+  i: Integer;
+  LUserInput: string;
+  LDecode, LEncode: Boolean;
+
+begin
+  LUserInput := '';
+
+  if ParamCount = 0 then
+  begin
+    while LUserInput <> 'quit' do
+    begin
+      Writeln('1. Encode (e)');
+      Writeln('2. Decode (d)');
+      Writeln('3. Quit (q)');
+      Write('> ');
+      Readln(LUserInput);
+
+      if (LUserInput = '1') or (LUserInput = 'e') or (LUserInput = 'encode') then
+      begin
+        while LUserInput <> 'back' do
+        begin
+          Writeln('');
+          Writeln('Enter string to encode or ''back'' to go back.');
+          Write('> ');
+          Readln(LUserInput);
+          if not LUserInput.IsEmpty then
+            Writeln(TNetEncoding.base64.Encode(LUserInput));
+        end;
+      end
+      else if (LUserInput = '2') or (LUserInput = 'd') or (LUserInput = 'decode') then
+      begin
+        while LUserInput <> 'back' do
+        begin
+          Writeln('');
+          Writeln('Enter string to encode or ''back'' to go back.');
+          Write('> ');
+          Readln(LUserInput);
+          if not LUserInput.IsEmpty then
+            Writeln(TNetEncoding.base64.Encode(LUserInput));
+        end;
+      end
+      else if (LUserInput = '3') or (LUserInput = 'q') or (LUserInput = 'quit') then
+      begin
+        Exit;
+      end;
+    end;
+  end
+  else if ParamCount = 2 then
+  begin
+    if ParamIsEncode(ParamStr(1)) then
+      Writeln(TNetEncoding.base64.Encode(ParamStr(2)))
+    else if ParamIsDecode(ParamStr(1)) then
+      Writeln(TNetEncoding.base64.Decode(ParamStr(2)))
+    else
+    begin
+      ShowHelp;
+    end;
+  end
+  else
+    ShowHelp;
+
+end.
